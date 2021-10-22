d6f5b29817c6a4fc092e92f017ee9f839878accb
lanhnv <lanhnv4@fe.edu.vn>
2021-10-22 21:49:39 +0700

    Express server 2

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express'),
-     http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
   console.log(req.headers);
@@ -13,6 +17,8 @@ app.use((req, res, next) => {
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
+
+
 
 const server = http.createServer(app);
 
